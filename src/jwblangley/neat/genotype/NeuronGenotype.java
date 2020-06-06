bb248a3eb5a1548373ebc77d58d3ac62ea86fab5
James Langley <jwblangley@gmail.com>
2020-06-07 00:20:37 +0100

    Implement NeuronGenotype
    And tests

--- src/jwblangley/neat/genotype/NeuronGenotype.java
+++ src/jwblangley/neat/genotype/NeuronGenotype.java
@@ -1,5 +1,59 @@
 package jwblangley.neat.genotype;
 
+import java.util.Objects;
+
+/**
+ * Genotype representing a neuron
+ */
 public class NeuronGenotype {
 
+  private final NeuronLayer layer;
+  private final int uid;
+
+  /**
+   * Constructs a new NeuronGenotype
+   * @param layer the layer in which this neuron resides
+   * @param uid unique identifier for this neuron
+   */
+  public NeuronGenotype(NeuronLayer layer, int uid) {
+    this.layer = layer;
+    this.uid = uid;
+  }
+
+  /**
+   * Copy constructor: creates a new NeuronGenotype object equal to toCopy
+   * @param toCopy NeuronGenotype to be copied
+   */
+  public NeuronGenotype(NeuronGenotype toCopy) {
+    this(toCopy.layer, toCopy.uid);
+  }
+
+  public NeuronLayer getLayer() {
+    return layer;
+  }
+
+  public int getUid() {
+    return uid;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    NeuronGenotype that = (NeuronGenotype) other;
+
+    assert this.uid != that.uid || this.layer == that.layer
+        : "equal neurons should have the same type";
+
+    return this.uid == that.uid;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(layer, uid);
+  }
 }
---
+++ src/jwblangley/neat/genotype/NeuronLayer.java
@@ -0,0 +1,7 @@
+package jwblangley.neat.genotype;
+
+public enum NeuronLayer {
+  INPUT,
+  HIDDEN,
+  OUTPUT
+}
