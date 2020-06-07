40a2fc4cd3ce9f4d77acb07b94d19309eb6a925f
James Langley <jwblangley@gmail.com>
2020-06-07 14:03:50 +0100

    Implement Evaluator

---
+++ src/jwblangley/neat/evolution/Evaluator.java
@@ -0,0 +1,15 @@
+package jwblangley.neat.evolution;
+
+import jwblangley.neat.genotype.NetworkGenotype;
+
+@FunctionalInterface
+public interface Evaluator {
+  /**
+   * Method to be implemented for the given optimisation that returns a fitness for a given
+   * genotype, usually by simulation. A strictly greater fitness must only be achieved if and
+   * only if the genotype was strictly better at solving the problem to be optimised.
+   * @param networkGenotype The genotype to be evaluated
+   * @return fitness: The fitness score
+   */
+  double evaluate(NetworkGenotype networkGenotype);
+}
